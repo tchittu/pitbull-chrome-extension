a63d400d9251ce96ca672d23509ed8a22d432a92
mayaef <m.e.franklin7@gmail.com>
2022-02-12 13:51:51 -0500

    icon

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 async function fetchData() {
-    const res=await fetch ("https://api.coronavirus.data.gov.uk/v1/data");
-    const covidRecord=await res.json();
-    document.getElementById("date").innerHTML=covidRecord.data[0].date;
-    document.getElementById("areaName").innerHTML=covidRecord.data[0].areaName;
-    document.getElementById("latestBy").innerHTML=covidRecord.data[0].latestBy;
-    document.getElementById("deathNew").innerHTML=covidRecord.data[0].deathNew;
+    chrome.browserAction.setIcon({
+        path : "green.png"
+    });
 }
 fetchData();
